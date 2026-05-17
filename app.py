807c2a22e8330dcad33b7544fe383b216b3b0d70
Rahaf Yaseen Barnawi <rahrfan.44426@gmail.com>
2026-05-17 16:38:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def emergency_action():
         "response": response
     })
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
